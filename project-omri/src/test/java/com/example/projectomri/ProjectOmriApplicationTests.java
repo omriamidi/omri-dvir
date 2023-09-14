9bc7aa5489338a921f200a7fbe0305ea1ec0a846
omriamidi <111886837+omriamidi@users.noreply.github.com>
2023-09-14 11:58:01 +0300

    add initial files for project

---
+++ project-omri/src/test/java/com/example/projectomri/ProjectOmriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectomri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOmriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
